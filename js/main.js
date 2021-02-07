26cd312dce0b431090cf36bf9fa7eb62a6b9209c
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-07 12:09:09 -0800

    Added in ability to click for cards to show up in DOM, create value for ech card as variable

--- js/main.js
+++ js/main.js
@@ -18,8 +18,43 @@ function getCards() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
+      let val1 = cardValue(data.cards[0].value)
+      let val2 = cardValue(data.cards[1].value)
+      document.querySelector('#player1').src = data.cards[0].image
+      document.querySelector('#player2').src = data.cards[1].image
+  
+      
+
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+
+}
+
+function cardValue(val){
+  switch (val) {
+    case 'ACE':
+      return 14
+      break;
+    case 'KING':
+      return 13
+      break;
+    case 'QUEEN':
+      return 12
+      break;
+    case 'JACK':
+      return 11
+    default:
+      break;
+  }
+  if (val === 'ACE'){
+    return 14
+  } else if (val === 'KING') {
+    return 13
+  } else if (val === 'QUEEN') {
+    return 12
+  } else if (val === 'JACK'){
+    return 11
+  }
 }
