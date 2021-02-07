077f06d772e36b522074327bcf7b783ed03738d2
Robby Noyes <robertjnoyes1@gmail.com>
2021-02-07 11:57:55 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//get the deck
+let deckId = ''
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    deckId = data.deck_id
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
+
+document.querySelector("button").addEventListener("click", getCards);
+
+function getCards() {
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
